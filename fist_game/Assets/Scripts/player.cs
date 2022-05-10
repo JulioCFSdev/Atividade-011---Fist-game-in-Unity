05a36278abbf484d3b36400bccf8c9e052f70fa4
JulioCFSdev <90019843+JulioCFSdev@users.noreply.github.com>
2022-05-09 23:25:07 -0300

    game update complete

--- fist_game/Assets/Scripts/player.cs
+++ fist_game/Assets/Scripts/player.cs
@@ -12,11 +12,13 @@ public class player : MonoBehaviour
     public bool doubleJump;
 
     private Rigidbody2D rig;
+    private Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -30,6 +32,23 @@ public class player : MonoBehaviour
     {
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime * Speed;
+
+        if(Input.GetAxis("Horizontal") > 0f)
+        {
+            anim.SetBool("walk", true);
+            transform.eulerAngles = new Vector3(0f, 0f, 0f);
+        }
+
+        if(Input.GetAxis("Horizontal") < 0f)
+        {
+            anim.SetBool("walk", true);
+            transform.eulerAngles = new Vector3(0f, 180f, 0f);
+        }
+
+        if(Input.GetAxis("Horizontal") == 0f)
+        {
+            anim.SetBool("walk", false);
+        }
     }
 
     void Jump()
@@ -40,7 +59,8 @@ public class player : MonoBehaviour
             if (!isJumping)
             {
                 rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
-                doubleJump = true;            
+                doubleJump = true;
+                anim.SetBool("jump", true);            
             }
 
             else
@@ -63,6 +83,7 @@ public class player : MonoBehaviour
         if (collision.gameObject.layer == 6)
         {
             isJumping = false;
+            anim.SetBool("jump", false);
         }
 
 
